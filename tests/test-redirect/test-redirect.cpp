b00f0ed39745e98eeb4f4812d0c5bff19a679642
Richard <legalize@xmission.com>
2024-05-01 15:44:54 -0600

    Initial commit for Boost.Url example

---
+++ tests/test-redirect/test-redirect.cpp
@@ -0,0 +1,8 @@
+#include <redirect/redirect.h>
+
+#include <gtest/gtest.h>
+
+TEST(TestRedirect, basic)
+{
+    ASSERT_TRUE(true);
+}
---
+++ tools/url-redirect/main.cpp
@@ -0,0 +1,6 @@
+#include <redirect/redirect.h>
+
+int main()
+{
+    return 0;
+}
